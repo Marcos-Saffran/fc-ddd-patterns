d7097f603d354acf880f23d5f6c14622462134ab
Marcos Alfredo Saffran <marcossaffran@gmail.com>
2026-03-05 15:52:27 -0300

    Update OrderRepository to implement OrderRepositoryInterface and add update method; add test for updating an order

--- src/infrastructure/order/repository/sequilize/order.repository.spec.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.spec.ts
@@ -81,4 +81,73 @@ describe("Order repository test", () => {
       ],
     });
   });
+
+  it("should update an order", async () => { 
+    const customerRepository = new CustomerRepository();
+    const customer = new Customer("123", "Customer 1");
+    const address = new Address("Street 1", 1, "Zipcode 1", "City 1");
+    customer.changeAddress(address);
+    await customerRepository.create(customer);
+
+    const productRepository = new ProductRepository();
+    const product = new Product("123", "Product 1", 10);
+    await productRepository.create(product);
+
+    const orderItem = new OrderItem(
+      "1",
+      product.name,
+      product.price,
+      product.id,
+      2
+    );
+
+    const order = new Order("123", "123", [orderItem]);
+
+    const orderRepository = new OrderRepository();
+    await orderRepository.create(order);
+
+    // change customer
+    const newCustomer = new Customer("124", "Customer 2");
+    const newAddress = new Address("Street 2", 2, "Zipcode 2", "City 2");
+    newCustomer.changeAddress(newAddress);
+    await customerRepository.create(newCustomer);
+
+    // change product
+    const newProduct = new Product("124", "Product 2", 20);
+    await productRepository.create(newProduct);
+
+    // change order item
+    const newOrderItem = new OrderItem(
+      "2",
+      newProduct.name,
+      newProduct.price,
+      newProduct.id,
+      3
+    );
+
+    const newOrder = new Order("123", "124", [newOrderItem]);
+
+    await orderRepository.update(newOrder);
+
+    const updatedOrderModel = await OrderModel.findOne({
+      where: { id: order.id },
+      include: ["items"],
+    });
+    
+    expect(updatedOrderModel.toJSON()).toStrictEqual({
+      id: "123",
+      customer_id: "124",
+      total: newOrder.total(),
+      items: [
+        {
+          id: newOrderItem.id,
+          name: newOrderItem.name,
+          price: newOrderItem.price,
+          quantity: newOrderItem.quantity,
+          order_id: "123",
+          product_id: "124",
+        },
+      ],
+    });
+  });
 });
