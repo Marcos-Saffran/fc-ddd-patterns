67f0d5ae58342698e17d5584b2dbe984d761e9d3
Marcos Alfredo Saffran <marcossaffran@gmail.com>
2026-03-05 16:34:51 -0300

    Implement find method in OrderRepository and add corresponding test case

--- src/infrastructure/order/repository/sequilize/order.repository.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -1,4 +1,5 @@
 import Order from "../../../../domain/checkout/entity/order";
+import OrderItem from "../../../../domain/checkout/entity/order_item";
 import OrderRepositoryInterface from "../../../../domain/checkout/repository/order-repository.interface";
 import OrderItemModel from "./order-item.model";
 import OrderModel from "./order.model";
@@ -51,8 +52,17 @@ export default class OrderRepository implements OrderRepositoryInterface {
     await OrderItemModel.bulkCreate(orderItems);
   }
 
-  find(id: string): Promise<Order> {
-    throw new Error("Method not implemented.");
+  async find(id: string): Promise<Order> {
+    const orderModel = await OrderModel.findOne({
+      where: { id },
+      include: ["items"],
+    });
+
+    const orderItems = orderModel.items.map((item) => 
+      new OrderItem(item.id, item.name, item.price, item.product_id, item.quantity)
+    );
+
+    return new Order(orderModel.id, orderModel.customer_id, orderItems);
   }
 
   findAll(): Promise<Order[]> {
