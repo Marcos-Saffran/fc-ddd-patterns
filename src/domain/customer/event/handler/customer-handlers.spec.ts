be1e1119251b6a6af52e8e7de1116feb0dfc1f6c
Marcos Alfredo Saffran <marcossaffran@gmail.com>
2026-03-11 09:25:47 -0300

    Add second event handler for CustomerCreated event and update tests

--- src/domain/customer/event/handler/customer-handlers.spec.ts
+++ src/domain/customer/event/handler/customer-handlers.spec.ts
@@ -1,9 +1,10 @@
 import EventDispatcher from "../../../@shared/event/event-dispatcher";
-import EnviaConsoleLog1Handler from "./envia-console-log-1.handlers";
+import EnviaConsoleLog1Handler from "./envia-console-log-1.handler";
+import EnviaConsoleLog2Handler from "./envia-console-log-2.handler";
 
-describe("Customer Domain Events Handlers Tests", () => { 
+describe("Customer Domain Events Handlers Tests", () => {
 
-    it("should register CustomerCreated event handler 1", () => { 
+    it("should register CustomerCreated event handler 1", () => {
 
         const eventDispatcher = new EventDispatcher();
         const eventHandler = new EnviaConsoleLog1Handler();
@@ -14,4 +15,31 @@ describe("Customer Domain Events Handlers Tests", () => {
         expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"].length).toBe(1);
         expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]).toMatchObject(eventHandler);
     });
+
+    it("should register CustomerCreated event handler 2", () => {
+
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new EnviaConsoleLog2Handler();
+
+        eventDispatcher.register("CustomerCreatedEvent", eventHandler);
+
+        expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"]).toBeDefined();
+        expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"].length).toBe(1);
+        expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]).toMatchObject(eventHandler);
+    });
+
+    it("should register both CustomerCreated event handlers", () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler1 = new EnviaConsoleLog1Handler();
+        const eventHandler2 = new EnviaConsoleLog2Handler();
+
+        eventDispatcher.register("CustomerCreatedEvent", eventHandler1);
+        eventDispatcher.register("CustomerCreatedEvent", eventHandler2);
+
+        expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"]).toBeDefined();
+        expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"].length).toBe(2);
+        expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]).toMatchObject(eventHandler1);
+        expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"][1]).toMatchObject(eventHandler2);
+    });
+    
 });
